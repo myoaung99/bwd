9bebd27cd8157d4abe92b881bcd4ec3e3bf901a6
MyoMyintAung <myomyintaung064@gmail.com>
2021-12-09 20:36:10 +0630

    Javascript Projects

---
+++ bwd_js.js
@@ -0,0 +1,55 @@
+// let myRecord = [];
+
+// const calcArea = (x,y) => {
+//     area = x * y;
+//     result = {
+//         "width": x,
+//         "height": y,
+//         area,
+//     }
+//     myRecord.push(result);
+//     return area;
+// }
+
+let rate = {
+    "usd": 1636,
+    "sgd": 1933,
+    "eur": 1203
+}
+
+
+function exchangeT0MMK(amount, currency){
+    let calc = amount * rate[currency];
+    return calc + " MMK";
+}
+
+function exchangeFromMMK(amount, currency){
+    let calc = Math.round(amount / rate[currency]);
+    return calc +" "+ currency;
+}
+
+let fruitPrice = {
+    orange: 100,
+    mango: 50,
+    apple: 700
+}
+
+let record = {};
+
+function calcTax(amount, percentage = 5){
+    return (amount * 5 ) / 100;
+}
+
+
+function cost(fruit, qty){
+    let calc = fruitPrice[fruit] * qty;
+    console.log(calc)
+    return {
+        fruit: fruit,
+        quatity: qty,
+        cost: calc,
+        tax: calcTax(calc),
+        total: calc + calcTax(calc)
+    };
+
+}
